c170447daa5f00ed0f4c4173e8b6c62295510d69
shorya1wd <shoryabhushan0@gmail.com>
2026-06-08 22:20:49 +0530

    add use effect for auto refresh compress size

--- app/(app)/video-upload/page.tsx
+++ app/(app)/video-upload/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
